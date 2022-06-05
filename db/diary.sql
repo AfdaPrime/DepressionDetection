069a34cd67ca668a2f159849c638897f592e4506
Afda <afiq.danish306@gmail.com>
2022-06-06 03:53:17 +0800

    Finish the login interface

--- db/diary.sql
+++ db/diary.sql
@@ -1,5 +1,6 @@
 -- Create table for the database
 -- CREATE TABLE IF NOT EXISTS diary (id INTEGER PRIMARY KEY,content TEXT NOT NULL,time TEXT not NULL,depression_level FLOAT NOT NULL);
+-- CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY,name TEXT NOT NULL,password TEXT not NULL);
 
 -- querying data fram table(see data from data base)
 SELECT * FROM diary;
@@ -9,3 +10,5 @@ SELECT * FROM diary;
 
 -- delete table
 -- DROP TABLE diary;
+
+-- SELECT password FROM user 
